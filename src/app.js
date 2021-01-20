a8f52f0ff45e3962e840d7e3a5531ae4042c5c99
HQDOMAIN\Jay.Govani <jay.govani@asurion.com>
2021-01-20 22:54:12 +0530

    setup app for heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const path = require('path')
 const hbs = require('hbs')
 const app = express()
 
+const port = process.env.PORT || 3000
 // setup path for express config
 const publicDirectoryPath = path.join(__dirname,'../public')
 const viewsPath = path.join(__dirname,'../templates/views')
@@ -95,6 +96,6 @@ app.get('*',(req,res)=>{
         name: 'Jay Govani'
     })
 })
-app.listen(3000, ()=>{
-    console.log('server is up on port 3000')
+app.listen(port, ()=>{
+    console.log('server is up on port '+ port)
 })
